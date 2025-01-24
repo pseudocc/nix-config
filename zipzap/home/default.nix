c7afdba98f1ab003a8bcb3ce90f4ec3311273e07
Atlas Yu <pseudoc@163.com>
2025-01-24 16:45:51 +0800

    feat(zsh): try zsh, bye bash

--- zipzap/home/default.nix
+++ zipzap/home/default.nix
@@ -45,8 +45,52 @@
   };
 
   programs = {
-    bash.enable = true;
+    command-not-found.enable = true;
+    fzf.enable = true;
+    starship.enable = true;
     home-manager.enable = true;
+    gh.enable = true;
+    bun.enable = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "bun"
+        "gh"
+        "fzf"
+        "vi-mode"
+        "git-prompt"
+        "git-auto-fetch"
+        "starship"
+        "command-not-found"
+        "history-substring-search"
+        "dotenv"
+      ];
+    };
+
+    shellAliases = {
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "rebuild" = "sudo nixos-rebuild --flake $HOME/nix-config#zipzap";
+    };
+
+    history = {
+      size = 2000;
+      ignoreAllDups = true;
+      ignorePatterns = [
+        "rm *"
+        "cd *"
+        "cp *"
+        "pkill *"
+      ];
+    };
   };
 
   programs.neovim = {
