41d4c137b4868dd0106a1c4e49dc5ac5e749ccde
Atlas Yu <pseudoc@163.com>
2025-02-08 22:43:08 +0800

    feat(ime): fcitx5 pinyin

--- zipzap/home/default.nix
+++ zipzap/home/default.nix
@@ -43,6 +43,16 @@ in {
     };
   };
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-gtk
+      fcitx5-configtool
+      fcitx5-chinese-addons
+      fcitx5-pinyin-zhwiki
+    ];
+  };
+
   programs = {
     home-manager.enable = true;
     gh.enable = true;
