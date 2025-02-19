c9812e0d29e81b6015d5fc1857ea6b954635770c
Atlas Yu <pseudoc@163.com>
2025-02-19 14:18:24 +0800

    style(nvim): cmp completion window

--- zipzap/home/nvim/plugins/cmp.nix
+++ zipzap/home/nvim/plugins/cmp.nix
@@ -21,6 +21,66 @@
       "<C-Down>" = "cmp.mapping.scroll_docs(4)";
     };
 
+    settings.window = let
+      base = {
+        border = "single";
+        side_padding = 1;
+        col_offset = 0;
+        zindex = 1001;
+        scrollbar = true;
+        scroffoff = 0;
+        winhighlight = lib.concatStringsSep "," [
+          "Normal:Normal"
+          "FloatBorder:FloatBorder"
+          "CursorLine:Visual"
+          "Search:None"
+        ];
+      };
+      mkWindow = overlay: base // overlay;
+    in {
+      completion = mkWindow {
+        max_height = 15;
+        max_width = 40;
+      };
+      documentation = mkWindow {
+        max_height = 15;
+        max_width = 60;
+        zindex = 1002;
+      };
+    };
+
+    settings.formatting.fields = [
+      "abbr"
+      "menu"
+      "kind"
+    ];
+    settings.formatting.format = let
+      label = {
+        max = "20";
+        min = "5";
+      };
+      menu = {
+        min = "0";
+        max = "10";
+      };
+      ellipsis = "…";
+    in ''
+      function (entry, item)
+        local function truncate(str, min, max)
+          str = str or ""
+          if #str > max then
+            return str:sub(0, max) .. "${ellipsis}"
+          elseif #str < min then
+            return str .. string.rep(" ", min - #str)
+          end
+          return str
+        end
+        item.menu = truncate(item.menu, ${menu.min}, ${menu.max})
+        item.abbr = truncate(item.abbr, ${label.min}, ${label.max})
+        return item
+      end
+    '';
+
     settings.snippet.expand = ''function (args)
       require('luasnip').lsp_expand(args.body)
     end'';
