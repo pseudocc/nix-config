12656382c8e9807d189c5004ad4f98d880d36eb7
Atlas Yu <pseudoc@163.com>
2025-02-24 09:19:07 +0800

    feat(git): enable lfs support

--- zipzap/home/git.nix
+++ zipzap/home/git.nix
@@ -3,6 +3,7 @@
 {
   programs.git = {
     enable = true;
+    lfs.enable = true;
     extraConfig = {
       user = { inherit (flakes.me) nick name email signingKey; };
       init.defaultBranch = "main";
