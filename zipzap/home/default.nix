ad25460ca1b8df210e558e51bf0268f7046ee59a
Atlas Yu <pseudoc@163.com>
2026-08-13 16:44:43 +0800

    feat(im): fcitx5.settings

--- zipzap/home/default.nix
+++ zipzap/home/default.nix
@@ -103,6 +103,28 @@ in {
       qt6Packages.fcitx5-chinese-addons
       fcitx5-pinyin-zhwiki
     ];
+    fcitx5.settings.addons = {
+      pinyin.globalSection = {
+        PageSize = 6;
+        CloudPinyinEnabled = "True";
+        CloudPinyinIndex = 2;
+      };
+      cloudpinyin.globalSection.Backend = "Baidu";
+    };
+    fcitx5.settings.inputMethod = {
+      GroupOrder."0" = "Default";
+      "Groups/0" = {
+        "Default Layout" = "us";
+        DefaultIM = "pinyin";
+        Name = "Default";
+      };
+      "Groups/0/Items/0".Name = "keyboard-us";
+      "Groups/0/Items/1".Name = "pinyin";
+    };
+    fcitx5.settings.globalOptions = {
+      "Hotkey/TriggerKeys"."0" = "Super+space";
+    };
+    fcitx5.waylandFrontend = true;
   };
 
   programs = {
